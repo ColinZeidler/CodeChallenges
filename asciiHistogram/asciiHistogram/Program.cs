b8732ccb44a4ee1ccdba5c52d8dc83fe6995d6de
Colin Zeidler <c.zeidler.wc@gmail.com>
2017-02-10 20:47:10 -0500

    asciiHistogram challenge in c#

---
+++ asciiHistogram/asciiHistogram/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asciiHistogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] graphbounds;
+            int lineCount, count;
+            Console.Write("> ");
+            string input = Console.ReadLine();
+
+            graphbounds = new int[4];
+            count = 0;
+            foreach (string item in input.Split())
+            {
+                graphbounds[count] = int.Parse(item);
+                count++;
+            }
+
+            foreach (int item in graphbounds)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+            Console.Write("\n");
+
+            input = Console.ReadLine();
+            lineCount = int.Parse(input);
+        }
+    }
+}
---
+++ asciiHistogram/asciiHistogram/graph.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asciiHistogram
+{
+    class Graph
+    {
+        public Graph(int xStart, int xEnd, int yStart, int yEnd)
+        {
+
+        }
+
+        public void printGraph()
+        {
+
+        }
+    }
+}
